b41ccf970d936e6c122fdb18f442974979a3a175
Jon McKenna <jonmckenna@Developers-MacBook-Pro-9.local>
2017-09-22 14:53:22 +0100

    First local change

--- AD-1090/run.ps1
+++ AD-1090/run.ps1
@@ -3,9 +3,9 @@ $requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $name = $requestBody.name
 
 # GET method: each querystring parameter is its own variable
-if ($req_query_name) 
+if ($req_query_name)
 {
-    $name = $req_query_name 
+    $name = $req_query_name
 }
 
-Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $name"
+Out-File -Encoding Ascii -FilePath $res -inputObject "Hello World $name"
